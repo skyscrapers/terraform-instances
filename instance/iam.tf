21a85a47f7079bd0bb1aec94be454ef08f80608f
Thomas Van den Berge <thomas@skyscrapers.eu>
2017-06-29 15:59:43 +0200

    Do not create IAM roles if no instances are created

--- instance/iam.tf
+++ instance/iam.tf
@@ -1,11 +1,12 @@
 resource "aws_iam_instance_profile" "profile" {
   name  = "profile_${var.name}_${var.project}_${var.environment}"
-  role = "${aws_iam_role.role.name}"
+  count = "${var.instance_count == "0" ? 0 : 1}"
+  role  = "${aws_iam_role.role.name}"
 }
 
 resource "aws_iam_role" "role" {
-  name = "role_${var.name}_${var.project}_${var.environment}"
-
+  name  = "role_${var.name}_${var.project}_${var.environment}"
+  count = "${var.instance_count == "0" ? 0 : 1}"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
