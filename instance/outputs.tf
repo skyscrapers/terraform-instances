daeda2bbfd1df1be92741590763e6e95969dc8a8
Sam Clinckspoor <sam.clinckspoor@gmail.com>
2017-08-03 13:39:54 +0200

    make ebs management in the instance optional

--- instance/outputs.tf
+++ instance/outputs.tf
@@ -1,45 +1,45 @@
 output "instance_ids" {
-  value = ["${aws_instance.instance.*.id}"]
+  value = ["${var.ebs_enabled ? aws_instance.instance.*.id : aws_instance.instance_no_ebs.*.id}"]
 }
 
 output "instance_azs" {
-  value = ["${aws_instance.instance.*.availability_zone}"]
+  value = ["${var.ebs_enabled ? aws_instance.instance.*.availability_zone : aws_instance.instance_no_ebs.*.availability_zone}"]
 }
 
 output "instance_placement_groups" {
-  value = ["${aws_instance.instance.*.placement_group}"]
+  value = ["${var.ebs_enabled ? aws_instance.instance.*.placement_group : aws_instance.instance_no_ebs.*.placement_group}"]
 }
 
 output "instance_key_names" {
-  value = ["${aws_instance.instance.*.key_name}"]
+  value = ["${var.ebs_enabled ? aws_instance.instance.*.key_name : aws_instance.instance_no_ebs.*.key_name}"]
 }
 
 output "instance_public_dns" {
-  value = ["${aws_instance.instance.*.public_dns}"]
+  value = ["${var.ebs_enabled ? aws_instance.instance.*.public_dns : aws_instance.instance_no_ebs.*.public_dns}"]
 }
 
 output "instance_public_ips" {
-  value = ["${aws_instance.instance.*.public_ip}"]
+  value = ["${var.ebs_enabled ? aws_instance.instance.*.public_ip : aws_instance.instance_no_ebs.*.public_ip}"]
 }
 
 output "instance_network_interface_ids" {
-  value = ["${aws_instance.instance.*.network_interface_id}"]
+  value = ["${var.ebs_enabled ? aws_instance.instance.*.network_interface_id : aws_instance.instance_no_ebs.*.network_interface_id}"]
 }
 
 output "instance_private_dns" {
-  value = ["${aws_instance.instance.*.private_dns}"]
+  value = ["${var.ebs_enabled ? aws_instance.instance.*.private_dns : aws_instance.instance_no_ebs.*.private_dns}"]
 }
 
 output "instance_private_ip" {
-  value = ["${aws_instance.instance.*.private_ip}"]
+  value = ["${var.ebs_enabled ? aws_instance.instance.*.private_ip : aws_instance.instance_no_ebs.*.private_ip}"]
 }
 
 output "instance_vpc_security_group_ids" {
-  value = ["${aws_instance.instance.*.instance_vpc_security_group_ids}"]
+  value = ["${var.ebs_enabled ? aws_instance.instance.*.instance_vpc_security_group_ids : aws_instance.instance_no_ebs.*.instance_vpc_security_group_ids}"]
 }
 
 output "instance_subnet_ids" {
-  value = ["${aws_instance.instance.*.subnet_id}"]
+  value = ["${var.ebs_enabled ? aws_instance.instance.*.subnet_id : aws_instance.instance_no_ebs.*.subnet_id}"]
 }
 
 output "role_id" {
