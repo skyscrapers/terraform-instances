98515df199e72a9e467106c4a56b6ecbde8f10c6
Sam Clinckspoor <sam.clinckspoor@gmail.com>
2017-08-03 16:10:29 +0200

    use concat for outputs

--- instance/outputs.tf
+++ instance/outputs.tf
@@ -1,45 +1,45 @@
 output "instance_ids" {
-  value = ["${var.ebs_enabled ? aws_instance.instance.*.id : aws_instance.instance_no_ebs.*.id}"]
+  value = ["${concat(aws_instance.instance.*.id,aws_instance.instance_no_ebs.*.id)}"]
 }
 
 output "instance_azs" {
-  value = ["${var.ebs_enabled ? aws_instance.instance.*.availability_zone : aws_instance.instance_no_ebs.*.availability_zone}"]
+  value = ["${concat(aws_instance.instance.*.availability_zone, aws_instance.instance_no_ebs.*.availability_zone) }"]
 }
 
 output "instance_placement_groups" {
-  value = ["${var.ebs_enabled ? aws_instance.instance.*.placement_group : aws_instance.instance_no_ebs.*.placement_group}"]
+  value = ["${concat(aws_instance.instance.*.placement_group, aws_instance.instance_no_ebs.*.placement_group)}"]
 }
 
 output "instance_key_names" {
-  value = ["${var.ebs_enabled ? aws_instance.instance.*.key_name : aws_instance.instance_no_ebs.*.key_name}"]
+  value = ["${concat(aws_instance.instance.*.key_name, aws_instance.instance_no_ebs.*.key_name)}"]
 }
 
 output "instance_public_dns" {
-  value = ["${var.ebs_enabled ? aws_instance.instance.*.public_dns : aws_instance.instance_no_ebs.*.public_dns}"]
+  value = ["${concat(aws_instance.instance.*.public_dns, aws_instance.instance_no_ebs.*.public_dns)}"]
 }
 
 output "instance_public_ips" {
-  value = ["${var.ebs_enabled ? aws_instance.instance.*.public_ip : aws_instance.instance_no_ebs.*.public_ip}"]
+  value = ["${concat(aws_instance.instance.*.public_ip, aws_instance.instance_no_ebs.*.public_ip)}"]
 }
 
 output "instance_network_interface_ids" {
-  value = ["${var.ebs_enabled ? aws_instance.instance.*.network_interface_id : aws_instance.instance_no_ebs.*.network_interface_id}"]
+  value = ["${concat(aws_instance.instance.*.network_interface_id, aws_instance.instance_no_ebs.*.network_interface_id)}"]
 }
 
 output "instance_private_dns" {
-  value = ["${var.ebs_enabled ? aws_instance.instance.*.private_dns : aws_instance.instance_no_ebs.*.private_dns}"]
+  value = ["${concat(aws_instance.instance.*.private_dns, aws_instance.instance_no_ebs.*.private_dns)}"]
 }
 
 output "instance_private_ip" {
-  value = ["${var.ebs_enabled ? aws_instance.instance.*.private_ip : aws_instance.instance_no_ebs.*.private_ip}"]
+  value = ["${concat(aws_instance.instance.*.private_ip, aws_instance.instance_no_ebs.*.private_ip)}"]
 }
 
 output "instance_vpc_security_group_ids" {
-  value = ["${var.ebs_enabled ? aws_instance.instance.*.instance_vpc_security_group_ids : aws_instance.instance_no_ebs.*.instance_vpc_security_group_ids}"]
+  value = ["${concat(aws_instance.instance.*.instance_vpc_security_group_ids, aws_instance.instance_no_ebs.*.instance_vpc_security_group_ids)}"]
 }
 
 output "instance_subnet_ids" {
-  value = ["${var.ebs_enabled ? aws_instance.instance.*.subnet_id : aws_instance.instance_no_ebs.*.subnet_id}"]
+  value = ["${concat(aws_instance.instance.*.subnet_id, aws_instance.instance_no_ebs.*.subnet_id) }"]
 }
 
 output "role_id" {
