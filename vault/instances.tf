d8eaf48cf7a3f10d9b7f06fe3b8c81ed335fbe50
Sam Clinckspoor <sam.clinckspoor@gmail.com>
2017-11-21 10:02:15 +0100

    Adds the Vault component (#15)
    * Adds the Vault component
    
    Adds the vault component module and documentation.

---
+++ vault/instances.tf
@@ -0,0 +1,25 @@
+module "vault1" {
+  source        = "github.com/skyscrapers/terraform-instances//instance?ref=1.3.1"
+  project       = "${var.project}"
+  environment   = "${terraform.workspace}"
+  name          = "vault1"
+  sgs           = ["${aws_security_group.vault.id}", "${var.teleport_node_sg}"]
+  subnets       = ["${var.vault1_subnet}"]
+  key_name      = "${var.key_name}"
+  ami           = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  user_data     = ["${data.template_cloudinit_config.vault1.rendered}"]
+}
+
+module "vault2" {
+  source        = "github.com/skyscrapers/terraform-instances//instance?ref=1.3.1"
+  project       = "${var.project}"
+  environment   = "${terraform.workspace}"
+  name          = "vault2"
+  sgs           = ["${aws_security_group.vault.id}", "${var.teleport_node_sg}"]
+  subnets       = ["${var.vault2_subnet}"]
+  key_name      = "${var.key_name}"
+  ami           = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  user_data     = ["${data.template_cloudinit_config.vault2.rendered}"]
+}
