6a667d6992a238bc5468a419eca8b8f9595ab897
Philip Dubois <hello@philipdubois.be>
2018-04-23 11:57:12 +0200

    Replace var.ebs_optimized_list leftover by the new module

--- instance/main.tf
+++ instance/main.tf
@@ -1,3 +1,8 @@
+module "is_ebs_optimised" {
+  source        = "../is_ebs_optimised"
+  instance_type = "${var.instance_type}"
+}
+
 resource "aws_instance" "instance" {
   count                       = "${var.ebs_enabled ? var.instance_count : 0}"
   ami                         = "${var.ami}"
@@ -7,7 +12,7 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids      = ["${var.sgs}"]
   subnet_id                   = "${element(var.subnets, count.index)}"
   disable_api_termination     = "${var.termination_protection}"
-  ebs_optimized               = "${contains(var.ebs_optimized_list,var.instance_type)}"
+  ebs_optimized               = "${module.is_ebs_optimised.is_ebs_optimised}"
   associate_public_ip_address = "${var.public_ip}"
   user_data                   = "${element(var.user_data, count.index)}"
 
@@ -29,11 +34,6 @@ resource "aws_instance" "instance" {
   lifecycle {
     ignore_changes = ["key_name", "user_data"]
   }
-}
-
-module "is_ebs_optimised" {
-  source        = "../is_ebs_optimised"
-  instance_type = "${var.instance_type}"
 }
 
 resource "aws_instance" "instance_no_ebs" {
